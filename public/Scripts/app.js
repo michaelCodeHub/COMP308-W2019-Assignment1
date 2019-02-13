452fbb162b8d188af04ee16279952e4a0cbc3a8c
Michael <michaeljaison.me@gmail.com>
2019-02-13 17:01:04 -0500

    Redesigned home page

--- public/Scripts/app.js
+++ public/Scripts/app.js
@@ -3,6 +3,7 @@
 // Student Number : 300958145
 // Date : Feb 12, 2019
 
+// Open slider menu
 function openNav() {
     if(document.getElementById("mySidenav").style.width == "400px"){
         document.getElementById("mySidenav").style.width = "0";
@@ -10,7 +11,7 @@ function openNav() {
         document.getElementById("mySidenav").style.width = "400px";
     }
   }
-  
+  // Close Slider menu
   function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
   }
